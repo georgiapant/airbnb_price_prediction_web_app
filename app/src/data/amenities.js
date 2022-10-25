efe006295922755fe47097b820d02f0303485a06
athinam6 <athina2658@gmail.com>
2022-10-25 14:58:09 +0300

    amenities list

---
+++ app/src/data/amenities.js
@@ -0,0 +1,32 @@
+export const amenities = [
+
+    {id: 1, val: "Kitchen"},
+    {id: 2, val: "Air conditioning"},
+    {id: 3, val: "High end electronics"},
+    {id: 4, val: "Barbeque"},
+    {id: 5, val: "Balcony"},
+    {id: 6, val: "Nature and views"},
+    {id: 7, val: "Bed linen"},
+    {id: 8, val: "Breakfast"},
+    {id: 9, val: "Tv"},
+    {id: 10, val: "Coffee machine"},
+    {id: 11, val: "Cooking basics"},
+    {id: 12, val: "Elevator"},
+    {id: 13, val: "Gym"},
+    {id: 14, val: "Child friendly"},
+    {id: 15, val: "Parking"},
+    {id: 16, val: "Outdoor space"},
+    {id: 17, val:"Host greeting"},
+    {id: 18, val:"Hot tub sauna or pool"},
+    {id: 19, val:"Internet"},
+    {id: 20, val:"Long term stays"},
+    {id: 21, val:"Pets allowed"},
+    {id: 22, val:"Private entrance"},
+    {id: 23, val:"Secure"},
+    {id: 24, val:"Self check-in"},
+    {id: 25, val:"Smoking allowed"},
+    {id: 26, val:"Accessible"},
+    {id: 27, val:"Event suitable"}
+
+]
+
