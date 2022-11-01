3fa8bd2661d82b4dfd5c22fa99a6bb491f1787ab
Georgia Pantalona <georgiapantalona@192.168.1.7>
2022-11-01 20:47:32 +0200

    team member info update

--- app/src/data/teamMembers.js
+++ app/src/data/teamMembers.js
@@ -3,21 +3,22 @@ export default [
   {
     name: "Athina Mousia",
     description:
-      "Hi, I’m a ...",
+      "Hi! I am an enthusiast postgraduate student on business analytics and data science. I graduated from the department of Mathematics  at Aristotle University of Thessaloniki.",
   },
   {
     name: "Dimitris Romios",
     description:
-      "Hi, I’m a ...",
+      "I am an inquisitive maths graduate who embraces challenges as an opportunity to problem-solve and to broaden my knowledge in the field of data, I joined the master program 'Data Science & Machine Learning' at the Hellenic Open University.",
   },
   {
     name: "Evgenia Moustridi",
     description:
-      "Hi, I’m a ...",
+      "I graduated from the Electrical and Computer Engineering Department of the Aristotle University of Thessaloniki and received my MSc in Biomedical Engineering from the University of Patras. In 2020 I joined the Visualization and Virtual Reality Group as a research associate. My main research interests are Data Science, Machine Learning and Biomechanics."
+      
   },
   {
     name: "Georgia Pantalona",
     description:
-      "...is aspiring data scientist.",
+      "I am an aspiring NLP researcher, currently working in the field of emotion analysis. I have obtained my integrated masters degree on Electrical and Computer Engineering as well as my Masters of Business Administration from the Aristotle University of Thessaloniki.",
   },
 ];
