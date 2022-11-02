19f23c93868cf6bd776c52a1dd1210d725adaf5f
evgmoustridi@gmail.com <evgmoustridi@gmail.com>
2022-11-02 22:34:51 +0200

    app, model update

--- api/controllers/model_controller.py
+++ api/controllers/model_controller.py
@@ -12,19 +12,6 @@ model_api = Blueprint(
     url_prefix="/api/v1.0/model"
 )
 
-# auth = HTTPBasicAuth()
-
-# users = {
-#     "johndoe": generate_password_hash("johndoe"),
-#     "python": generate_password_hash("my_python")
-# }
-
-
-# @auth.verify_password
-# def verify_password(username, password):
-#     if username in users and check_password_hash(users.get(username), password):
-#         return username
-
 @model_api.errorhandler(404)
 def not_found(error):
     return jsonify({"error": "Not found. Your fault"}), 404
@@ -44,30 +31,7 @@ def forbidden(error):
     return jsonify({"error": "Internal server error. My fault"}), 403
 
 
-# @auth.error_handler
-# def unauthorized():
-#     # return 403 instead of 401 to prevent browsers from displaying the default authy dialog,
-#     # this way we can handle AUTH errors/exceptions
-#     return jsonify({'error': 'Unauthorized access'}), 403
-
-
-# Model User Posts  some necessary data, we return the reponse containing 
-# listing info and the predicted price
-# req = {
-#     "id":2818,
-#     "host_id":3159,
-#     "latitude":52.36435,
-#     "longitude":4.94358,
-#     "room_type":"Private room",
-#     "minimum_nights":3,
-#     "accomodates": 2,
-#     "beds": 1,
-#     "has_wifi": 1,
-#     }
-
-
 @model_api.route('/', methods=['POST'])
-# @auth.login_required
 def model():
     '''
     Check that all fields are filled
@@ -94,6 +58,5 @@ def model():
         # abort(404)
     if result.get('error'):
         abort(500)
-    # logger.error(result)
 
     return jsonify(result), 200
